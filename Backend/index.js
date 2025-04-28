b825ea9927f70d744f1643052890a367615d877b
abutalibhaidry <ahaidry110@gmail.com>
2025-04-28 13:59:57 +0530

    yes

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middlewares
 app.use(cors({
